79a2b6cc9dd0f01a2f05910dc310e41903d4ff85
sreenandan-1246 <shiva.parise8518@gmail.com>
2025-01-03 18:03:42 +0530

    username entered

--- 1205-up.sh
+++ 1205-up.sh
@@ -4,13 +4,13 @@ echo "Please enter the user name"
 
 read -s USERNAME
 
-echo "print the $USERNAME"
+echo "username added $USERNAME"
 
 echo "Please enter the password"
 
-read PASSWORD
+read -s PASSWORD
 
-echo "print the $PASSWORD"
+echo "password added $PASSWORD"
 
 
 
