73148fc553e8c0182c32f0e7d6356e490f29e666
sreenandan-1246 <shiva.parise8518@gmail.com>
2025-01-03 16:33:40 +0530

    1201 hellow world script needs to be added

---
+++ 1201-hellow-world.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Hi Yamuna"
+echo "How are you doing"
+echo "Hi Dhansihka"
+echo "How was your studies are going on?"
+echo "Hi Sreenandan"
+echo "Are you playing in the ground?"
+
