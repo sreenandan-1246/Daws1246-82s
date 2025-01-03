13a2c7f6b6bee37c37751733c85c5fd758517e55
sreenandan-1246 <shiva.parise8518@gmail.com>
2025-01-03 16:45:58 +0530

    Assign Variables

---
+++ 1202-AS.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Yamuna
+Person2=Shiva
+
+echo "$PERSON1::Hi $PERSON2"
+echo "$PERSON2::Hello $PERSON1"
+echo "$PERSON1::How are you? $PERSON2"
+echo "$PERSON2::I am good, How about you $PERSON1"
