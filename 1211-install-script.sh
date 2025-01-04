541abbf228bb20e352df3dec121b3d776c9c8a17
sreenandan-1246 <shiva.parise8518@gmail.com>
2025-01-04 23:32:13 +0530

    1211 install mysql and git

--- 1211-install-script.sh
+++ 1211-install-script.sh
@@ -7,10 +7,6 @@ then
     exit 1
 fi
 
-dnf list installed mysql
-
-if [ $? -ne 0 ]
-then
 
 dnf install mysql -y
 
@@ -22,31 +18,15 @@ else
      echo "Installation is.... Success"
 fi
 
-else
-
-echo "mysql is already .....installed"
-
-fi
-
-
-dnf list installed git
-
-if [ $? -ne 0 ]
-then
 
 dnf install git -y
 
 if [ $? -ne 0 ]
 then
-     echo "Installation git... Failure"
+     echo "Installing git... Failure"
      exit 1
 else
-     echo "Installation git.... Success"
+     echo "Installing git.... Success"
 fi
 
-else
-
-echo "git is already.... installed"
-
-fi
 
