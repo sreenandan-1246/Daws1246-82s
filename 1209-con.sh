d5ca840d5e4f38a2715d4a10d9505d002bef8e37
sreenandan-1246 <shiva.parise8518@gmail.com>
2025-01-04 15:02:26 +0530

    1209 conditions needs to be passed

--- 1209-con.sh
+++ 1209-con.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 NUMBER=$1
-if($NUMBER -gt 100)
-
+if[$NUMBER -gt 100]
 then
-
-echo "Given number is greater than 100"
-
+echo "Given Number is greater then 100"
 else
-
-echo "Given number is less than or equal to 100"
-
+echo "Given Number is less than or equal to 100"
 fi
