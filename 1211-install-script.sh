a1f504d4c9f89d674909823ca326dcc3f127132f
sreenandan-1246 <shiva.parise8518@gmail.com>
2025-01-04 23:11:07 +0530

    1209 uptodate

--- 1211-install-script.sh
+++ 1211-install-script.sh
@@ -7,7 +7,41 @@ then
     exit 1
 fi
 
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+
 dnf install mysql -y
 
+if [ $? -ne 0 ]
+then
+     echo "Installation mysql... Failure"
+     exit 1
+else
+     echo "Installation is.... Success"
+fi
+
+else
+
+echo "mysql is already .....installed"
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+
 dnf install git -y
+
+if [ $? -ne 0 ]
+then
+     echo "Installation git... Failure"
+     exit 1
+else
+     echo "Installation git.... Success"
+fi
+
+else
+echo "git is already.... installed"
+fi
 
