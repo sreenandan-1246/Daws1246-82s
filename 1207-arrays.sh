4f5ee5bad7d8bec96a4317391b0ea88ebcc92410
sreenandan-1246 <shiva.parise8518@gmail.com>
2025-01-04 13:00:24 +0530

    arrays added

---
+++ 1207-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FAMILY=("shiva" "yamuna" "dhanishka" "sreenandan")
+
+echo "Father of the house:c${FAMILY[0]}"
+echo "Mother of the house: ${FAMILY[1]}"
+echo "First child of the house: ${FAMILY[2]}"
+echo "Second child of the house: ${FAMILY[3]}"
+
+echo "All Family members in the house: ${FAMILY[@]}"
+
