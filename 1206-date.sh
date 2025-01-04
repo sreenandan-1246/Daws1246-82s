0e72f74c174300eb7614f02c9223586635fbd22e
sreenandan-1246 <shiva.parise8518@gmail.com>
2025-01-04 11:21:23 +0530

    ds

--- 1206-date.sh
+++ 1206-date.sh
@@ -5,10 +5,9 @@ NUMBER=$2
 
 TIMESTAMP=$(date)
 
-echo "script executed at: $TIMESTAMP"
+echo "Script is executed: $TIMESTAMP"
 
 SUM=$(($NUMBER1+$NUMBER2))
 
-echo "Sum of $NUMBER1 and $NUMBER2 is: $SUM"
-
+echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
 
